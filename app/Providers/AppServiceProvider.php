eac68f4a94aa6177f9ba56558e8bb651dc469a81
Anh Van <anhvan@example.com>
2025-09-17 11:59:52 +0700

    thêm if AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,9 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-{
-   
-}
+    {
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
+    }
 }
