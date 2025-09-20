abdafdfe7978988b51e74a7ad6adb5ca1da1458e
Ho Gia Huy <huyhg101004@gmail.com>
2025-09-20 22:34:24 +0700

    fix

--- config/mail.php
+++ config/mail.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'default' => env('MAIL_MAILER', 'sendmail'),
+    'default' => env('MAIL_MAILER', 'log'),
 
     /*
     |--------------------------------------------------------------------------
