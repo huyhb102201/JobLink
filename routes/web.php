766d9fad96033e674db0716daac563e017e92f22
Ho Gia Huy <huyhg101004@gmail.com>
2025-10-03 09:56:33 +0700

    fix

--- routes/web.php
+++ routes/web.php
@@ -122,20 +122,6 @@ Route::middleware('auth')->prefix('settings')->name('settings.')->group(function
     Route::get('/tax', [SettingsController::class, 'tax'])->name('tax');
     Route::get('/connected', [SettingsController::class, 'connected'])->name('connected');
     Route::get('/appeals', [SettingsController::class, 'appeals'])->name('appeals');
-    Route::get('/payment/success', [CheckoutController::class, 'paymentSuccess'])->name('payment.success');
-Route::get('/payment/cancel', [CheckoutController::class, 'paymentCancel'])->name('payment.cancel');
-
-Route::get('/settings/company',  [CompanyController::class, 'index'])->name('settings.company');
-Route::post('/settings/company', [CompanyController::class, 'store'])->name('settings.company.store');
-Route::post('/settings/company/members/add', [CompanyController::class, 'addMemberByUsername'])
-        ->name('company.members.add');
-Route::post('/settings/company/members/invite', [CompanyController::class, 'inviteByUsername'])
-        ->name('company.members.invite');
-Route::get('/invite/{token}', [CompanyController::class, 'acceptInvite'])
-    ->name('company.invite.accept');
-
-Route::delete('/settings/company/{org}/member/{account}', [CompanyController::class, 'removeMember'])
-    ->name('company.members.remove');
 });
 // Routes Xác minh Email
 Route::get('/email/verify', function () {
@@ -222,3 +208,17 @@ Route::middleware(['auth', 'role:ADMIN'])->prefix('admin')->name('admin.')->grou
         Route::put('/accounts/{id}', [AccountController::class, 'update'])->name('accounts.update');
     });
 
+Route::get('/payment/success', [CheckoutController::class, 'paymentSuccess'])->name('payment.success');
+Route::get('/payment/cancel', [CheckoutController::class, 'paymentCancel'])->name('payment.cancel');
+
+Route::get('/settings/company',  [CompanyController::class, 'index'])->name('settings.company');
+Route::post('/settings/company', [CompanyController::class, 'store'])->name('settings.company.store');
+Route::post('/settings/company/members/add', [CompanyController::class, 'addMemberByUsername'])
+        ->name('company.members.add');
+Route::post('/settings/company/members/invite', [CompanyController::class, 'inviteByUsername'])
+        ->name('company.members.invite');
+Route::get('/invite/{token}', [CompanyController::class, 'acceptInvite'])
+    ->name('company.invite.accept');
+
+Route::delete('/settings/company/{org}/member/{account}', [CompanyController::class, 'removeMember'])
+    ->name('company.members.remove');
